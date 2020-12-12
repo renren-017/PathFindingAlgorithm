e69e1ba30612751647be0c7b0d566a9b475cffb0
3xxxact <amir260303@gmail.com>
2020-12-12 19:33:56 +0600

    update_1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import math
+from queue import PriorityQueue
+
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
@@ -70,17 +73,38 @@ class Node:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neigh(self, grid):
-        pass
+        self.neighbours = []
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_brrier():
+            self.neighbours.append(grid[self.row + 1][self.col])
+        if self.row > 0 and not grid[self.row - 1][self.col].is_brrier():
+            self.neighbours.append(grid[self.row + 1][self.col])
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_brrier():
+            self.neighbours.append(grid[self.row][self.col + 1])
+        if self.row > 0 and not grid[self.row][self.col - 1].is_brrier():
+            self.neighbours.append(grid[self.row][self.col - 1])
 
     def __lt__(self, other):
         return False
 
 
-def hf(p1, p2):
+def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
 
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {node: float('inf') for row in grid for node in row}
+    g_score[start] = 0
+    f_score = {node: float('inf') for row in grid for node in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+    #1:16
 
 def make_grid(rows, width):
     grid = []
@@ -96,9 +120,9 @@ def make_grid(rows, width):
 def draw_grid(win, rows, width):
     node_w = width // rows
     for i in range(rows):
-        pygame.draw.line(win, GREY, (0, i * node_w), (width, i * node_w))
+        pygame.draw.line(win, BLACK, (0, i * node_w), (width, i * node_w))
         for j in range(rows):
-            pygame.draw.line(win, GREY, (j * node_w, 0), (j * node_w, width))
+            pygame.draw.line(win, BLACK, (j * node_w, 0), (j * node_w, width))
 
 
 def draw(win, grid, rows, width):
@@ -167,7 +191,13 @@ def main(win, width):
                 elif node == end:
                     end = None
 
-    pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors()
+                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
 
+    pygame.quit()
 
 main(WIN, WIDTH)
